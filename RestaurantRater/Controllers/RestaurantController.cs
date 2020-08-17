a69154d9c94097426acc197202f9b61d19f44802
AThooker <austintrent@icloud.com>
2020-08-17 17:29:37 -0400

    rater complete

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -87,5 +87,19 @@ namespace RestaurantRater.Controllers
             }
             return View();
         }
+        //Get: Restaurant/Get/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
